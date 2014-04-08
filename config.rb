7fb454096fcccc20828732e56bf061a7651c0aad
Luiz Tanure <letanure@gmail.com>
2014-04-08 01:41:09 -0300

    add middleman deploy to gh-pages

--- config.rb
+++ config.rb
@@ -77,3 +77,8 @@ configure :build do
   # Or use a different image path
   # set :http_path, "/Content/images/"
 end
+
+activate :deploy do |deploy|
+  deploy.method = :git
+  deploy.build_before = true
+end
